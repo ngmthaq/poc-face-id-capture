6f62e422f10d851519356d73a297dbd684720b77
Victor Nguyen <nmthang6@cmcglobal.vn>
2026-05-13 11:39:08 +0700

    fix: reorder exports in index.ts for consistency

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-export { default as FaceRegister } from "./templates/FaceRegister";
 export type {
   Capture,
   FaceRegisterProps,
   FaceRegisterTranslations,
 } from "./templates/FaceRegister";
+
 export type { StepName } from "./shared/types/faceRegister";
+
+export { default as FaceRegister } from "./templates/FaceRegister";
